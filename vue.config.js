d8b8c37a571ca166e718847446c2603de1577386
shiliangl <1462999406@qq.com>
2021-07-17 18:41:25 +0800

    add files x2021年07月17日x

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
+/*
+ * @Author: shiliangL
+ * @Date: 2021-07-17 18:32:22
+ * @LastEditTime: 2021-07-17 18:40:32
+ * @LastEditors: Do not edit
+ * @Description:
+ */
 module.exports = {
-  publicPath: './',
+  publicPath: './vue-3d-research/',
   outputDir: 'dist',
   assetsDir: 'static',
   productionSourceMap: false,
